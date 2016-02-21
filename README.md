1f350d03efbeaf250a1f143a0cc1a56050e15a6d
Darren Kavanagh <dkav@users.noreply.github.com>
2016-02-21 13:44:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# xkcd Graphs
+xkcd styled graphs created in d3, Python and R.
+
+Python and R graphs are coded within a Jupyter notebook.
+
+Python graph requires matplotlib.
+
+R graph requires ggplot2 and xkcd packages. R code is executed via rpy2. To setup R locally, prep_r.R needs to be run first.
+
+Daniel Foreman-Mackey created the [d3 style](http://dan.iel.fm/xkcd/).
+Emilio Torres-Manzanera is the maintainer of the R [xkcd package](https://cran.r-project.org/web/packages/xkcd/index.html).
+Michael Droettboom created the [xkcd sketch-style drawing mode](http://matplotlib.org/users/whats_new.html#xkcd-style-sketch-plotting) for matplotlib.
---
+++ jupyter/prep_r.R
@@ -0,0 +1,29 @@
+# Setup local copy of R to use xkcd font
+
+# Function to install package if not present
+pkgTest <- function(pkg) {
+  if (!require(pkg,character.only = TRUE)) {
+    install.packages(pkg,dep=TRUE)
+    if(!require(pkg,character.only = TRUE)) stop("Package not found")
+  }
+}
+
+# Install xkcd font
+# download.file("http://simonsoftware.se/other/xkcd.ttf", dest="xkcd.ttf", mode="wb")
+system("cp xkcd.ttf ~/Library/Fonts")
+system("rm xkcd.ttf")
+pkgTest("sysfonts")
+library(sysfonts)
+stopifnot("xkcd.ttf" %in% font.files())
+
+# Setup font for R
+pkgTest("extrafont")
+library(extrafont)
+font_import(pattern = "[X/x]kcd", prompt=FALSE)
+fonts()
+fonttable()
+loadfonts
+
+# Test if required libraries are present
+pkgTest("ggplot2")
+pkgTest("xkcd")
